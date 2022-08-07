845f6ead9012728f68ab5fc4c7eb3b6760ebca54
= <=>
2022-08-07 17:52:05 +0900

    0807 study

--- blog/main.py
+++ blog/main.py
@@ -1,74 +1,103 @@
-from hashlib import new
-from urllib import response
-from fastapi import FastAPI, Depends, status, Response, HTTPException
 # from pydantic import BaseModel
-from . import schemas, models
-from .database import SessionLocal, engine
-from sqlalchemy.orm import Session
-
-models.Base.metadata.create_all(engine)
-
-def get_db() :
-    db = SessionLocal()
-    try :
-        yield db
-    finally :
-        db.close()
+from fastapi import FastAPI
+from . import models
+from .database import engine
+from .routers import blog, user, authentication
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
+
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
+
+# deta
+# Deploy scalable Node & Python apps in seconds.
+# ― 수 초 내로 확장 가능한 Node 및 Python 앱 배포
+
+
+# def get_db() :
+#     db = SessionLocal()
+#     try :
+#         yield db
+#     finally :
+#         db.close()
+
+
 # uvicorn blog.main:app --reload
-@app.post("/blog", status_code=status.HTTP_201_CREATED) # 브라우저의 전송 코드를 제어할 수 있다. status_code=201
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    # return request
-    # return db
-    new_blog = models.Blog(title=request.title, body=request.body)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+# @app.post("/blog", status_code=status.HTTP_201_CREATED, tags=['blogs']) # 브라우저의 전송 코드를 제어할 수 있다. status_code=201
+# def create(request: schemas.Blog, db: Session = Depends(get_db)):
+#     # return request
+#     # return db
+#     new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
+#     db.add(new_blog)
+#     db.commit()
+#     db.refresh(new_blog)
+#     return new_blog
 
-@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session = Depends(get_db)) :
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
+# @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=['blogs'])
+# def destroy(id, db: Session = Depends(get_db)) :
+#     blog = db.query(models.Blog).filter(models.Blog.id == id)
 
-    if not blog.first() :
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+#     if not blog.first() :
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
 
-    blog.delete(synchronize_session=False)
-    db.commit()
+#     blog.delete(synchronize_session=False)
+#     db.commit()
 
-    return 'done'
+#     return 'done'
 
-@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    #print(type(request))
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
+# @app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
+# def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+#     #print(type(request))
+#     blog = db.query(models.Blog).filter(models.Blog.id == id)
     
-    if not blog.first() :
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+#     if not blog.first() :
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
     
-    blog.update(request.dict())
-    db.commit()
-    return 'updated'
+#     blog.update(request.dict())
+#     db.commit()
+#     return 'updated'
 
 
 
-@app.get("/blog")
-def all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+# @app.get("/blog", response_model=List[schemas.ShowBlog], tags=['blogs'])
+# def all(db: Session = Depends(get_db)):
+#     blogs = db.query(models.Blog).all()
+#     return blogs
 
 
-@app.get("/blog/{id}", status_code=200)
-def show(id, response:Response, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog :
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not available")
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {'detatil': f"Blog with the id {id} is not available"}
+# @app.get("/blog/{id}", status_code=200, response_model=schemas.ShowBlog, tags=['blogs'])
+# def show(id, response:Response, db: Session = Depends(get_db)):
+#     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+#     if not blog :
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not available")
+#         # response.status_code = status.HTTP_404_NOT_FOUND
+#         # return {'detatil': f"Blog with the id {id} is not available"}
 
-    return blog
+#     return blog
 
 
+
+# @app.post('/user', response_model=schemas.ShowUser, tags=['users'])
+# def create_user(request: schemas.User, db: Session = Depends(get_db)):
+#     new_user = models.User(
+#         name=request.name,
+#         email=request.email,
+#         password=Hash.bcrypt(request.password)
+#         )
+#     db.add(new_user)
+#     db.commit()
+#     db.refresh(new_user)
+#     return new_user
+
+
+# @app.get('/user/{id}', response_model=schemas.ShowUser, tags=['users'])
+# def get_user(id:int, db: Session = Depends(get_db)) :
+#     user = db.query(models.User).filter(models.User.id == id).first()
+#     if not user :
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} not found")
+    
+#     return user
 
